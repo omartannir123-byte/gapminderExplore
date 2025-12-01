fd8743a0ef311015533b46926160b1e61a773f22
Omar Eltannir <omartannir123@gmail.com>
2025-11-30 19:10:33 -0500

    Delete R directory

--- R/summarize_life_expectancy.R
+++
@@ -1,8 +0,0 @@
-#' Summarize Life Expectancy by Year
-#'
-#' @export
-summarize_life_expectancy <- function(data) {
-  data %>%
-    dplyr::group_by(year) %>%
-    dplyr::summarise(mean_lifeExp = mean(lifeExp, na.rm = TRUE))
-}
